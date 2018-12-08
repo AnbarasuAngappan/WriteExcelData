734f75b359eef3f533aaacd9677dcd5090b9ec9e
Anbarasu Angappan <kanbuarasu27@gmail.com>
2018-12-08 17:20:04 +0530

    New Changes<<---

---
+++ ConsoleAppTest/ModBus.cs
@@ -0,0 +1,131 @@
+﻿using FieldTalk.Modbus.Master;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest
+{
+    class ModBus
+    {
+        private static MbusMasterFunctions myProtocol;
+        private int retryCnt;
+        private int pollDelay;
+        private int timeOut;
+        private int tcpPort;
+        private int res;
+
+        public ModBus()
+        {
+            myProtocol = new MbusRtuOverTcpMasterProtocol();//
+            try
+            {
+                retryCnt = int.Parse("");
+            }
+            catch (Exception)
+            {
+                retryCnt = 0;
+            }
+            try
+            {
+                pollDelay = int.Parse("");
+            }
+            catch (Exception)
+            {
+                pollDelay = 0;
+            }
+            try
+            {
+                timeOut = int.Parse("1000");
+            }
+            catch (Exception)
+            {
+                timeOut = 1000;
+            }
+
+            myProtocol.timeout = timeOut;
+            myProtocol.retryCnt = retryCnt;
+            myProtocol.pollDelay = pollDelay;
+
+        }
+
+        public bool OpenProtocal(string _ipAddress, int _port)
+        {
+            try
+            {
+                tcpPort = _port;
+                ((MbusRtuOverTcpMasterProtocol)myProtocol).port = (short)tcpPort;
+                res = ((MbusRtuOverTcpMasterProtocol)myProtocol).openProtocol(_ipAddress);
+
+                if ((res == BusProtocolErrors.FTALK_SUCCESS))
+                {
+
+                    return true;
+
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+
+        }
+
+        public short[] ReadHoldingregister(string _slaveAddress, string _strAddress, string _noReadRegister)
+        {
+            int slave;
+            int startRdReg;
+            short[] readVals = new short[125];
+            int numRdRegs;
+            try
+            {
+                slave = int.Parse(_slaveAddress);
+            }
+            catch (Exception)
+            {
+                slave = 1;
+            }
+
+            try
+            {
+                startRdReg = int.Parse(_strAddress);
+            }
+            catch (Exception)
+            {
+                startRdReg = 1;
+            }
+
+            try
+            {
+                numRdRegs = int.Parse(_noReadRegister);
+            }
+            catch (Exception)
+            {
+                numRdRegs = 1;
+            }
+
+            try
+            {
+                res = myProtocol.readMultipleRegisters(slave, startRdReg, readVals, numRdRegs);
+                if (readVals != null)
+                {
+                    return readVals;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ ConsoleAppTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;using System.Linq;using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest
+{
+    class Program
+    {
+        ModBus modbusReader;
+        static void Main(string[] args)
+        {
+
+            Program program = new Program();
+            program.Test();
+        }
+
+        public void Test()
+        {
+            modbusReader = new ModBus();
+            bool asss = modbusReader.OpenProtocal("192.168.5.178", 502);
+        }
+    }
+}
